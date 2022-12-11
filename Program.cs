fedec17164900a63c802ed982340cab4aba4c7f1
gokhancuryan <119778787+gokhancuryan@users.noreply.github.com>
2022-12-11 18:47:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time >= 6 && time <=11)
+                Console.WriteLine("Have a Good Morning");
+            else if(time > 11 && time <= 17)
+                Console.WriteLine("Have a Good Day");
+            else if(time > 17)
+                Console.WriteLine("Have a Nice Night");
+            
+            Console.WriteLine(DateTime.Now);
+
+            string sonuc = time <= 18 ? "iyi günler" : "İyi geceler";
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+            
+        }
+    }
+}
